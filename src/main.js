54c7dd2e63771c5d3970fa921c44f5d6196b254f
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-11-28 17:51:22 -0300

    iniciando aplicação Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+// Vue
+new Vue ({
+    el: '#container-pai',
+    data: {
+        titulo: "sei lá",
+
+    },
+})
+
+
 let nav = document.getElementById('navbar');
 let imgList = document.getElementById('img-lista');
+let imgUser = document.getElementById("img-user");
 nav.addEventListener('mouseenter', entradaMouse);
 nav.addEventListener('mouseleave', saidaMouse);
 
@@ -7,9 +18,12 @@ nav.addEventListener('mouseleave', saidaMouse);
 function entradaMouse(){
     nav.style.backgroundColor = "black";
     imgList.style.filter = "invert()";
+    imgUser.style.filter = "invert()";
 }
 
 function saidaMouse(){
     nav.style.backgroundColor = "rgba(0,0,0,0.5)";
     imgList.style.filter = "none";
+    imgUser.style.filter = "none";
 }
+
