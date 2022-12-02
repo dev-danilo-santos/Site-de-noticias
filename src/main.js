018b0e23199fb728b330fad728db459b509ab143
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-12-02 18:05:38 -0300

    populando notícias

--- src/main.js
+++ src/main.js
@@ -27,11 +27,12 @@ function criarNoticias(quantidade){
         noticia.titulo = `Titulo da noticia ${i}`
         noticia.autor = `Autor da noticia ${i}`
         noticia.texto = `Texto da noticia ${i}`
+        noticia.img = 
         listaNoticia.push(noticia)
     }
     return listaNoticia;
 }
-let listaDeNoticias = criarNoticias(10);
+let listaDeNoticias = criarNoticias(30);
 
 function renderizarNoticias(){
     for(let i = 0; i<listaDeNoticias.length; i++){
@@ -46,25 +47,47 @@ function renderizarNoticias(){
     textoNoticia.innerHTML = listaDeNoticias[i].texto;
     textoNoticia.className = 'texto-noticia';
     
-    if(i == 0) {
-        document.getElementById('noticia-principal-dia').appendChild(tituloNoticia);
-        document.getElementById('noticia-principal-dia').appendChild(autorNoticia);
-        document.getElementById('noticia-principal-dia').appendChild(textoNoticia);
-    } else if(i == 1) {
-        document.getElementById('primeira-noticia').appendChild(tituloNoticia);
-        document.getElementById('primeira-noticia').appendChild(autorNoticia);
-        document.getElementById('primeira-noticia').appendChild(textoNoticia);
-    } else if(i == 2) {
-        document.getElementById('segunda-noticia').appendChild(tituloNoticia);
-        document.getElementById('segunda-noticia').appendChild(autorNoticia);
-        document.getElementById('segunda-noticia').appendChild(textoNoticia);
-    }
-    else {
-        document.getElementById('coluna-direita').appendChild(tituloNoticia);
-        document.getElementById('coluna-direita').appendChild(autorNoticia);
-        document.getElementById('coluna-direita').appendChild(textoNoticia);
+        if(i == 0) {
+            document.getElementById('noticia-principal-dia').appendChild(tituloNoticia);
+            document.getElementById('noticia-principal-dia').appendChild(autorNoticia);
+            document.getElementById('noticia-principal-dia').appendChild(textoNoticia);
+        } else if(i == 1) {
+            document.getElementById('primeira-noticia').appendChild(tituloNoticia);
+            document.getElementById('primeira-noticia').appendChild(autorNoticia);
+            document.getElementById('primeira-noticia').appendChild(textoNoticia);
+        } else if(i == 2) {
+            document.getElementById('segunda-noticia').appendChild(tituloNoticia);
+            document.getElementById('segunda-noticia').appendChild(autorNoticia);
+            document.getElementById('segunda-noticia').appendChild(textoNoticia);
+        } else if(i < 7 ) {
+            document.getElementById('coluna-direita').appendChild(tituloNoticia);
+            document.getElementById('coluna-direita').appendChild(autorNoticia);
+            document.getElementById('coluna-direita').appendChild(textoNoticia); 
+        } else if(i < 11 ) {
+            document.getElementById('coluna-direita2').appendChild(tituloNoticia);
+            document.getElementById('coluna-direita2').appendChild(autorNoticia);
+            document.getElementById('coluna-direita2').appendChild(textoNoticia); 
+        } else if(i < 16 ) {
+            document.getElementById('coluna-direita3').appendChild(tituloNoticia);
+            document.getElementById('coluna-direita3').appendChild(autorNoticia);
+            document.getElementById('coluna-direita3').appendChild(textoNoticia); 
+        }
+        else {
+            document.getElementById('coluna-esquerda-sec').appendChild(tituloNoticia);
+            document.getElementById('coluna-esquerda-sec').appendChild(autorNoticia);
+            document.getElementById('coluna-esquerda-sec').appendChild(textoNoticia);
+        }
     }
 
 }
+
+function NoticiasDetalhadas(){
+    let noticiasJSON = '{"noticias":[{"titulo":"Opinião: Brasil tem reservas que seriam titulares em várias seleções","autor":"joao","imagem":"src/img/noticias/reservas-selecao.jpg","descricao":"Quarteto ofensivo de hoje é o mais jovem desde a era Pelé"},]}'
+    return noticiasJSON;
+}
+
+function renderizarNoticiasdetalhadas(){
+    let lista = JSON.parse(NoticiasDetalhadas());
+    console.log(typeof(lista))
 
 }
